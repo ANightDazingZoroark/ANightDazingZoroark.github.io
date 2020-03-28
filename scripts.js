f9bf36010e6490f8bbf2fab43550f9bffbe45a0c
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2020-03-28 21:00:28 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-    var dropdown = document.getElementsByClassName("dropdown-btn");
-    var i;
+ function popupModal(){
+ var dropdown = document.getElementsByClassName("dropdown-btn");
+ var i;
 
-    for (i = 0; i < dropdown.length; i++) {
-      dropdown[i].addEventListener("click", function() {
+ for (i = 0; i < dropdown.length; i++) {
+   dropdown[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var dropdownContent = this.nextElementSibling;
       if (dropdownContent.style.display === "block") {
@@ -13,3 +14,27 @@
       }
     });
   }
+}
+ 
+function openTab(evt, tabName) {
+  // Declare all variables
+  var t, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (t = 0; t < tabcontent.length; t++) {
+    tabcontent[t].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (t = 0; t < tablinks.length; t++) {
+    tablinks[t].className = tablinks[t].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  
+  document.getElementById("defaultOpen").click();
+}
